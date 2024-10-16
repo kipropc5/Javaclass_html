c36226de62baebe4f95078e251a99ea860d9a6c9
kipropc5 <kipropc5@gmail.com>
2024-10-16 15:35:58 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+//get the button element
+const javaclass=document.getElementById('colorButton');
+//add event listener for the button click
+javaclass.addEventListener('click',()=>{
+    //change the colour of the button
+   javaclass.style.backgroundColor=javaclass.style.backgroundColor==='blue'?'#4CAF50':'blue';
+});
